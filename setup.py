68104fc2c277fcefa44dfbcc490d2d5092655805
Jonas Baumann <jone@jone.ch>
2012-11-12 13:59:42 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(name='ftw.dashboard.dragndrop',
         "functionality to the dashboard",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from 
+
+      # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -22,22 +23,26 @@ setup(name='ftw.dashboard.dragndrop',
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+
+      keywords='ftw dashboard dragndrop',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='https://github.com/4teamwork/ftw.dashboard.dragndrop',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.dashboard'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
           'setuptools',
           'collective.js.jqueryui',
       ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
